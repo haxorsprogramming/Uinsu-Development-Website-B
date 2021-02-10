c67d3eac071cee8e0f908292578a05f62b772372
haxorsprogramming <haxorsuinsu@gmail.com>
2021-02-10 14:40:52 +0700

    update

---
+++ app/Http/Controllers/C_Home.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class C_Home extends Controller
+{
+    public function home()
+    {
+        return view('home.home');
+    }
+
+    public function about()
+    {
+        return view('home.home');
+    }
+}
